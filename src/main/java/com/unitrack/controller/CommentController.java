9b28decf2beff4d4425d9f11e643d0d76a6ea16c
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2025-10-30 15:06:16 +0100

    Implement task comment endpoints

--- src/main/java/com/unitrack/controller/CommentController.java
+++ src/main/java/com/unitrack/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.unitrack.controller;
 
 import com.unitrack.dto.request.CreateTaskDto;
 import com.unitrack.service.CommentService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +17,26 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping("/")
-    public void addComment(@RequestParam Long taskId, CreateTaskDto dto, Principal principal) {
+    public String addComment(@RequestParam Long taskId, CreateTaskDto dto, Principal principal, HttpServletRequest request) {
         commentService.addComment(taskId, dto, principal);
+
+        String referer = request.getHeader("Referer");
+        return "redirect:" + (referer != null ? referer : "/");
     }
 
     @PutMapping("/{id}")
-    public void updateComment(@PathVariable Long id, @RequestParam Long taskId, CreateTaskDto dto) {
+    public String updateComment(@PathVariable Long id, @RequestParam Long taskId, CreateTaskDto dto, HttpServletRequest request) {
         commentService.updateComment(id, taskId, dto);
+
+        String referer = request.getHeader("Referer");
+        return "redirect:" + (referer != null ? referer : "/");
     }
 
     @DeleteMapping("/{id}")
-    public void deleteComment(@PathVariable Long id) {
+    public String deleteComment(@PathVariable Long id, HttpServletRequest request) {
         commentService.deleteComment(id);
+
+        String referer = request.getHeader("Referer");
+        return "redirect:" + (referer != null ? referer : "/");
     }
 }
