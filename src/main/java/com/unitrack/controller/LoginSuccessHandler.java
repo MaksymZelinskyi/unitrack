35a800026f2dbb0aa69c0660507bcf85d88bcc5f
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2026-01-19 00:12:11 +0100

    Fix OIDC issues

--- src/main/java/com/unitrack/controller/LoginSuccessHandler.java
+++ src/main/java/com/unitrack/controller/LoginSuccessHandler.java
@@ -4,14 +4,17 @@ import com.unitrack.service.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.security.Principal;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
@@ -19,18 +22,28 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final AuthenticationService authenticationService;
 
     @Override
-    public void onAuthenticationSuccess(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Authentication authentication
-    ) throws IOException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
 
-        UserDetails user = (UserDetails) authentication.getPrincipal();
-
-        authenticationService.defineWorkspace(user.getUsername());
-
-        response.setStatus(HttpServletResponse.SC_OK);
+        String username = resolveUsername(authentication);
+        authenticationService.defineWorkspace(username);
+        log.debug("Authentication succeeded for user {}", username);
         response.sendRedirect("/home");
+    }
+
+    private String resolveUsername(Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof OidcUser oidcUser) {
+            return oidcUser.getEmail();
+        }
+        if (principal instanceof OAuth2User oAuth2User) {
+            return oAuth2User.getAttribute("email");
+        }
+        if (principal instanceof UserDetails user) {
+            return user.getUsername();
+        }
+        throw new IllegalStateException("Illegal principal type: " + (principal == null ? "None" : principal.getClass()));
     }
 }
 
