1cbb9bc49aedd91ae4bc1cf3595249e690419eff
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2026-06-17 16:42:26 +0200

    Add /home mapping back

--- src/main/java/com/unitrack/controller/HomeController.java
+++ src/main/java/com/unitrack/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -31,11 +32,15 @@ public class HomeController extends AuthenticatedController {
     private final ProjectService projectService;
 
     @GetMapping
-    public String home() {
+    public String root() {
         return "redirect:/home";
     }
 
+    @GetMapping("/home")
+    public String home(Model model) {
 
+        return "home";
+    }
 
 
 }
